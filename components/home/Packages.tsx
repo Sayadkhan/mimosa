4138fd65894ec96d36bceed1d472061bc20d6988
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-08-26 03:59:16 +0600

    section title added git push

---
+++ components/home/Packages.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SectionTitle from "../shared/SectionTitle";
+
+const Packages = () => {
+  return (
+    <section className="wrapper section-padding" id="packages">
+      <SectionTitle
+        title="Explore Our Beauty packages"
+        subtitle="Packages"
+        color="bg-red"
+      />
+    </section>
+  );
+};
+
+export default Packages;
