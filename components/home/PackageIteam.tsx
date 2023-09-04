e800fd5090a139a6f23550d339b15b9581a45c3f
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-09-04 08:24:02 +0600

    packageitem card added

--- components/home/PackageIteam.tsx
+++ components/home/PackageIteam.tsx
@@ -1,4 +1,8 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { buttonVariants } from '../ui/Button';
+import { formatCurrency } from '@/utils/formatCurrency';
 
 interface PackageIteamProps {
   id: number;
@@ -17,7 +21,39 @@ const PackageIteam: React.FC<PackageIteamProps> = ({
   description,
   price,
 }) => {
-  return <div>PackageIteam</div>;
+  return (
+    <div className='flex w-full flex-col gap-5'>
+      {/* image */}
+      <Link
+        className='group h-[15rem] w-full overflow-hidden rounded-2xl border border-grey'
+        href={`/packages/${id}`}
+      >
+        <Image
+          width={640}
+          height={640}
+          src={image}
+          alt={title}
+          priority
+          className='eoq h-full w-full object-cover brightness-90 group-hover:scale-125 group-hover:brightness-100'
+        />
+      </Link>
+      {/* contant */}
+      <div className='eoq flex w-full flex-col gap-2.5 rounded-2xl border bg-grey/25 p-5 hover:bg-grey/50'>
+        <span className='text-xs font-bold uppercase tracking-[0.375em] text-green'>
+          {category}
+        </span>
+        <h4 className='text-2xl'>{title}</h4>
+        <hr />
+        <p className='text-dark/75'>{description.substring(0, 50)}...</p>
+        <div className='flex items-center justify-between gap-2.5'>
+          <p>{formatCurrency(price)}</p>
+          <Link href={`/packages/${id}`} className={buttonVariants()}>
+            view details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PackageIteam;
